3c79c4fd4eb89b9d42c046131beb93fb27f7fe8b
tabohulu <bohulukwame@gmail.com>
2021-02-09 23:42:52 +0900

    adds contact form

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-'use strict'
+
 let header_links=document.getElementsByClassName("header-nav");
 let mobile_menu= document.getElementById('mobile-menu')
 let prevActiveDiv=""
@@ -13,7 +13,8 @@ function selectLink(event){
     changeColorOnClick(event)
     
     let curDiv=event.target.href.split('#')[1]
-    hideGrid(curDiv,prevActiveDiv)
+    console.log(event.target.href)
+    hideGrid(curDiv)
 
 }
 
@@ -25,17 +26,23 @@ function changeColorOnClick(event){
 
 }
 
-function hideGrid(curDiv,prevDiv){
+function hideGrid(curDiv){
+    console.log(curDiv)
     let activeDiv=document.getElementsByClassName(curDiv)[0];
-    let inactiveDiv=document.getElementsByClassName(prevDiv)[0]
-    console.log(activeDiv!=='undefined',inactiveDiv!=='undefined')
+
+    let inactiveDiv=document.getElementsByClassName('menu-divs')
+    for(var i=0;i<inactiveDiv.length;i++){
+        inactiveDiv[i].classList.add('hidden')
+    }
+    activeDiv.classList.remove('hidden')
+    /*console.log(activeDiv!=='undefined',inactiveDiv!=='undefined')
     
     if(curDiv!=prevDiv){
         if(typeof activeDiv!=='undefined')
         {activeDiv.classList.remove('hidden')}
         if(typeof inactiveDiv!=='undefined')
         {inactiveDiv.classList.add('hidden')}
-    }
+    }*/
     showMobileMenu()
     
 }
@@ -45,4 +52,22 @@ function showMobileMenu(){
     console.log(mobile_dropdown.style.display)
     mobile_dropdown.style.display==='block'?mobile_dropdown.style.display='none':mobile_dropdown.style.display='block'
 }
+
+//var request = new XMLHttpRequest()
+/*const proxyurl = "https://cors-anywhere.herokuapp.com/";
+let url="https://bad-api-assignment.reaktor.com/v2/products/facemasks"
+const url2="https://bad-api-assignment.reaktor.com/v2/availability/ippal"
+
+const request = new XMLHttpRequest();
+request.open("GET",proxyurl+ url);
+request.setRequestHeader("x-force-error-mode","all");
+request.send();
+request.onload = ()=>{
+    console.log(request);
+    if(request.status == 200 && request.response!=='') {
+        console.log(JSON.parse(request.response));
+    }else{
+        console.log('no response')
+    }
+}*/
 
